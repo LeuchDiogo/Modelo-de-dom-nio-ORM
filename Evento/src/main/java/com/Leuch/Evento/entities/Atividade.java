9a34e9d743d1ee815790adfe428dddc14cff04fd
LeuchDiogo <diogo14leuch@gmail.com>
2024-12-16 15:50:54 -0300

    Implementa associações entre classes

---
+++ Evento/src/main/java/com/Leuch/Evento/entities/Atividade.java
@@ -0,0 +1,96 @@
+package com.Leuch.Evento.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    @OneToMany(mappedBy = "atividade" , cascade = CascadeType.ALL)
+    private List<Bloco> blocos = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_atividade_participante",
+                joinColumns = @JoinColumn(name = "atividade_id"),
+                inverseJoinColumns = @JoinColumn(name = "participante_id"))
+    private Set<Participante> participantes = new HashSet<>();
+
+
+    public  Atividade () {}
+
+    public Atividade(Integer id, String nome, String descricao, Double price) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public List<Bloco> getBlocos() {
+        return blocos;
+    }
+
+    public Set<Participante> getParticipantes() {
+        return participantes;
+    }
+}
---
+++ Evento/src/main/java/com/Leuch/Evento/entities/Categoria.java
@@ -0,0 +1,49 @@
+package com.Leuch.Evento.entities;
+
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Atividade> atividade = new ArrayList<>();
+
+    public Categoria () {}
+
+    public Categoria(String descricao, Integer id) {
+        this.descricao = descricao;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Atividade> getAtividade() {
+        return atividade;
+    }
+}
